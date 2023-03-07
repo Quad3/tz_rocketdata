852c7e7d2bc90ecbb7a43543b4c718749cf2d5d9
Quad3 <bbulat1990@gmail.com>
2023-03-07 12:28:21 +0900

    views producer list filter by country

--- api/views.py
+++ api/views.py
@@ -5,6 +5,14 @@ from .serializers import ProducerSerializer
 
 
 class ProducerAPIView(generics.ListAPIView):
-    queryset = Producer.objects.all().prefetch_related('products')\
-        .select_related('contact', 'contact__address')
     serializer_class = ProducerSerializer
+    
+    def get_queryset(self):
+        queryset = Producer.objects.prefetch_related('products')\
+            .select_related('contact', 'contact__address')
+        country = self.request.query_params.get('country')
+        if country is not None:
+            queryset = Producer.objects.filter(contact__address__country=country).prefetch_related('products')\
+                .select_related('contact', 'contact__address')
+
+        return queryset
