ec6c44c8146060b21e0565d5952a6e96e767022b
Quad3 <bbulat1990@gmail.com>
2023-03-07 12:53:24 +0900

    ProducerAboveAverageDebtAPIView

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProducerAPIView
+from .views import ProducerAPIView, ProducerAboveAverageDebtAPIView
 
 
 urlpatterns = [
     path('producer', ProducerAPIView.as_view()),
+    path('producer-debt-above-average', ProducerAboveAverageDebtAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import generics
 
 from api.models import Producer
@@ -6,7 +7,7 @@ from .serializers import ProducerSerializer
 
 class ProducerAPIView(generics.ListAPIView):
     serializer_class = ProducerSerializer
-    
+
     def get_queryset(self):
         queryset = Producer.objects.prefetch_related('products')\
             .select_related('contact', 'contact__address')
@@ -14,5 +15,16 @@ class ProducerAPIView(generics.ListAPIView):
         if country is not None:
             queryset = Producer.objects.filter(contact__address__country=country).prefetch_related('products')\
                 .select_related('contact', 'contact__address')
+
+        return queryset
+
+
+class ProducerAboveAverageDebtAPIView(generics.ListAPIView):
+    serializer_class = ProducerSerializer
+
+    def get_queryset(self):
+        avg = Producer.objects.aggregate(Avg('debt'))['debt__avg']
+        queryset = Producer.objects.filter(debt__gte=avg).prefetch_related('products')\
+            .select_related('contact', 'contact__address')
 
         return queryset
